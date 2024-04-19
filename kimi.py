a05b49799786b1a29824caf2e40ac4f47d4e8f43
D1monik <116081418+D1monik@users.noreply.github.com>
2024-04-19 11:26:31 +0200

    class

--- kimi.py
+++ kimi.py
@@ -1,4 +1,20 @@
-class Kimi:
-    def __init__(self,):
+f = open("kimi.csv" "r" encoding="utf-8")
+f.readline
 
+
+class Kimi:
+    def __init__(self,Dat,NagyD,Hely,BefKor,Pont,Kon,Cel,KorH,Hiba):
+        self.Dat = Dat
+        self.NagyD = NagyD
+        self.Hely = Hely
+        self.BefKor = BefKor
+        self.Pont = Pont
+        self.Kon = Kon
+        self.Cel = Cel
+        self.KorH = KorH
+        self.Hiba = Hiba
         
+    def __str__(self):
+        return f"{self.Dat} {self.NagyD} {self.Hely} {self.BefKor} {self.Pont} {self.Kon} {self.Cel} {self.KorH} {self.Hiba}"
+
+
